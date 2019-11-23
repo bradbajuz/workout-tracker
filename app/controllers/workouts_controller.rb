82ed97484ca9d913c45255ecb3e5845981163026
Brad Ballard <brad@bradballard.com>
2019-11-23 09:56:43 -0500

    Build out workout form

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,8 +1,16 @@
 class WorkoutsController < ApplicationController
   def index
   end
-  
+
   def new
-    
+    @workout = Workout.new
+    @workout.exercise_sets.build
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:name,
+                                    exercise_sets_attributes: [:id, :workout_id, :adjustment_point, :date, :weight, :time])
   end
 end
