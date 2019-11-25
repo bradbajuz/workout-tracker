9d9ec77ca20c55213e70dcc8fb5f122df6716919
Brad Ballard <brad@bradballard.com>
2019-11-25 12:12:52 -0500

    Add ability to pick time

--- app/javascript/packs/flatpickr.js
+++ app/javascript/packs/flatpickr.js
@@ -3,4 +3,12 @@ document.addEventListener("turbolinks:load", () => {
     static: true,
     dateFormat: 'Y-m-d',
   });
+  flatpickr('.stopwatchpicker', {
+    enableTime: true,
+    time_24hr: true,
+    enableSeconds: true,
+    noCalendar: true,
+    dateFormat: 'H:i:S',
+    defaultDate: '00:00:00',
+  });
 });
