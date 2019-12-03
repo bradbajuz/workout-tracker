e956a34149fc3ec02ad57ee72d46e7627ad667c7
Brad Ballard <brad@bradballard.com>
2019-12-03 15:42:34 -0500

    Add ability to create a exercise set

--- app/controllers/exercise_sets_controller.rb
+++ app/controllers/exercise_sets_controller.rb
@@ -1,2 +1,23 @@
 class ExerciseSetsController < ApplicationController
+  def new
+    @exercise_set = ExerciseSet.new
+  end
+
+  def create
+    @exercise_set = ExerciseSet.new(exercise_set_params)
+
+    respond_to do |format|
+      if @exercise_set.save
+        format.html { redirect_to workouts_path, notice: "Exercise Set was successfully created." }
+      else
+        format.html { render :new, error: "Error creating exercise set. Please try again." }
+      end
+    end
+  end
+
+  private
+
+  def exercise_set_params
+    params.require(:exercise_set).permit(:workout_id, :exercise_id, :adjustment_point, :date, :weight, :time)
+  end
 end
